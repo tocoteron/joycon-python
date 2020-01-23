5a69f4a7df5d25d80cbc58c0c7bc1e62b84a11f6
atsukoba <atsuya_wgp4346@keio.jp>
2020-01-24 05:39:29 +0900

    pep8 format

--- pyjoycon/device.py
+++ pyjoycon/device.py
@@ -4,7 +4,7 @@ import hid
 def get_device_ids():
     """
     returns dictionary of id info
-    
+
     {
         "R": {
             "product": PRODUCT_ID,
@@ -28,18 +28,18 @@ def get_device_ids():
             print(f"vendor_id is {device['vendor_id']}")
 
             if "R" in prod:
-                  R["product"] = device['product_id']
-                  R["vendor"] = device['vendor_id']
+                R["product"] = device['product_id']
+                R["vendor"] = device['vendor_id']
             else:
-                  L["product"] = device['product_id']
-                  L["vendor"] = device['vendor_id']
+                L["product"] = device['product_id']
+                L["vendor"] = device['vendor_id']
     return {"R": R, "L": L}
 
 
 def get_ids(lr):
     """
     returns tuple of `(vendor_id, product_id)`
-    
+
     arg: lr : str : put `R` or `L`
     """
     ids = get_device_ids()
