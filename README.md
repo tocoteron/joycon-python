2a870eab0a5b07ba4033c41b9ebf480b5bb52499
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-03-28 23:19:46 +0100

    Add PythonicJoyCon wrapper class, make __init__ more pythonic
    Minor version was bumped, due to new feature

--- README.md
+++ README.md
@@ -23,11 +23,9 @@ python pyjoycon/joycon.py
 or use as module
 
 ```python
-from pyjoycon import device
-from pyjoycon.joycon import JoyCon
+from pyjoycon import JoyCon, get_R_id
 
-
-joycon_id = device.get_R_id()
+joycon_id = get_R_id()
 joycon = JoyCon(*joycon_id)
 
 joycon.get_status()
--- pyjoycon/__init__.py
+++ pyjoycon/__init__.py
@@ -1,1 +1,8 @@
-__version__ = "0.2.0"
+from .joycon import JoyCon
+from .wrappers import PythonicJoyCon # as JoyCon
+from .device import get_device_ids, get_ids_of_type
+from .device import is_id_L
+from .device import get_R_ids, get_L_ids
+from .device import get_R_id, get_L_id
+
+__version__ = "0.2.1"
