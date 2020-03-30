6ceb053c2f75dc99935ff53eb3104732c081d752
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-03-30 20:53:38 +0200

    Add __all__ to __init__.py

--- pyjoycon/__init__.py
+++ pyjoycon/__init__.py
@@ -8,3 +8,17 @@ from .device import get_R_ids, get_L_ids
 from .device import get_R_id, get_L_id
 
 __version__ = "0.2.3"
+
+__all__ = [
+    "ButtonEventJoyCon",
+    "GyroTrackingJoyCon",
+    "JoyCon",
+    "PythonicJoyCon",
+    "get_L_id",
+    "get_L_ids",
+    "get_R_id",
+    "get_R_ids",
+    "get_device_ids",
+    "get_ids_of_type",
+    "is_id_L",
+]
