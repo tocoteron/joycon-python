ae71ff6585ade81e6ba2972994406b41d18fa5d1
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-04-01 22:54:14 +0200

    Make the default writing-to-buffer behaviour of ButtonEventJoyCon overridable
    Fixup previous commit

--- pyjoycon/event.py
+++ pyjoycon/event.py
@@ -28,6 +28,9 @@ class ButtonEventJoyCon(PythonicJoyCon):
         else:
             self.register_update_hook(self._event_tracking_update_hook_right)
 
+    def joycon_button_event(self, button, state): # overridable
+        self._events_buffer.append((button, state))
+
     def events(self):
         while self._events_buffer:
             yield self._events_buffer.pop(0)
@@ -38,47 +41,47 @@ class ButtonEventJoyCon(PythonicJoyCon):
             pressed = self.stick_r_btn
             if self._previous_stick_r_btn != pressed:
                 self._previous_stick_r_btn = pressed
-                self._events_buffer.append(("stick_r_btn", pressed))
+                self.joycon_button_event("stick_r_btn", pressed)
         pressed = self.r
         if self._previous_r != pressed:
             self._previous_r = pressed
-            self._events_buffer.append(("r", pressed))
+            self.joycon_button_event("r", pressed)
         pressed = self.zr
         if self._previous_zr != pressed:
             self._previous_zr = pressed
-            self._events_buffer.append(("zr", pressed))
+            self.joycon_button_event("zr", pressed)
         pressed = self.plus
         if self._previous_plus != pressed:
             self._previous_plus = pressed
-            self._events_buffer.append(("plus", pressed))
+            self.joycon_button_event("plus", pressed)
         pressed = self.a
         if self._previous_a != pressed:
             self._previous_a = pressed
-            self._events_buffer.append(("a", pressed))
+            self.joycon_button_event("a", pressed)
         pressed = self.b
         if self._previous_b != pressed:
             self._previous_b = pressed
-            self._events_buffer.append(("b", pressed))
+            self.joycon_button_event("b", pressed)
         pressed = self.x
         if self._previous_x != pressed:
             self._previous_x = pressed
-            self._events_buffer.append(("x", pressed))
+            self.joycon_button_event("x", pressed)
         pressed = self.y
         if self._previous_y != pressed:
             self._previous_y = pressed
-            self._events_buffer.append(("y", pressed))
+            self.joycon_button_event("y", pressed)
         pressed = self.home
         if self._previous_home != pressed:
             self._previous_home = pressed
-            self._events_buffer.append(("home", pressed))
+            self.joycon_button_event("home", pressed)
         pressed = self.right_sr
         if self._previous_right_sr != pressed:
             self._previous_right_sr = pressed
-            self._events_buffer.append(("right_sr", pressed))
+            self.joycon_button_event("right_sr", pressed)
         pressed = self.right_sl
         if self._previous_right_sl != pressed:
             self._previous_right_sl = pressed
-            self._events_buffer.append(("right_sl", pressed))
+            self.joycon_button_event("right_sl", pressed)
 
     @staticmethod
     def _event_tracking_update_hook_left(self):
@@ -86,44 +89,44 @@ class ButtonEventJoyCon(PythonicJoyCon):
             pressed = self.stick_l_btn
             if self._previous_stick_l_btn != pressed:
                 self._previous_stick_l_btn = pressed
-                self._events_buffer.append(("stick_l_btn", pressed))
+                self.joycon_button_event("stick_l_btn", pressed)
         pressed = self.l
         if self._previous_l != pressed:
             self._previous_l = pressed
-            self._events_buffer.append(("l", pressed))
+            self.joycon_button_event("l", pressed)
         pressed = self.zl
         if self._previous_zl != pressed:
             self._previous_zl = pressed
-            self._events_buffer.append(("zl", pressed))
+            self.joycon_button_event("zl", pressed)
         pressed = self.minus
         if self._previous_minus != pressed:
             self._previous_minus = pressed
-            self._events_buffer.append(("minus", pressed))
+            self.joycon_button_event("minus", pressed)
         pressed = self.up
         if self._previous_up != pressed:
             self._previous_up = pressed
-            self._events_buffer.append(("up", pressed))
+            self.joycon_button_event("up", pressed)
         pressed = self.down
         if self._previous_down != pressed:
             self._previous_down = pressed
-            self._events_buffer.append(("down", pressed))
+            self.joycon_button_event("down", pressed)
         pressed = self.left
         if self._previous_left != pressed:
             self._previous_left = pressed
-            self._events_buffer.append(("left", pressed))
+            self.joycon_button_event("left", pressed)
         pressed = self.right
         if self._previous_right != pressed:
             self._previous_right = pressed
-            self._events_buffer.append(("right", pressed))
+            self.joycon_button_event("right", pressed)
         pressed = self.capture
         if self._previous_capture != pressed:
             self._previous_capture = pressed
-            self._events_buffer.append(("capture", pressed))
+            self.joycon_button_event("capture", pressed)
         pressed = self.left_sr
         if self._previous_left_sr != pressed:
             self._previous_left_sr = pressed
-            self._events_buffer.append(("left_sr", pressed))
+            self.joycon_button_event("left_sr", pressed)
         pressed = self.left_sl
         if self._previous_left_sl != pressed:
             self._previous_left_sl = pressed
-            self._events_buffer.append(("left_sl", pressed))
+            self.joycon_button_event("left_sl", pressed)
