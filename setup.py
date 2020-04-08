951ebfdbcb0ba565031790aebd79edd387f2989b
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-04-08 14:47:13 +0200

    fix dependecies in setup.py, add myself while at it

--- setup.py
+++ setup.py
@@ -8,17 +8,25 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='joycon-python',
     version=version,
     description='Python driver for Nintendo Switch Joy-Con',
     long_description=readme,
     long_description_content_type='text/markdown',
-    author='tokoroten-lab, atsukoba',
-    author_email='tokoroten.lab@gmail.com, atsuya_kobayashi@yahoo.co.jp',
+    author='tokoroten-lab, atsukoba, pbsds',
+    author_email=', '.join([
+        'tokoroten.lab@gmail.com'
+        'atsuya_kobayashi@yahoo.co.jp',
+        'pbsds@hotmail.com',
+    ]),
     url='https://github.com/tokoroten-lab/joycon-python',
     license=license,
     packages=find_packages(),
+    install_requires=requirements,
     classifiers=[
         'Programming Language :: Python :: 3.7'
     ]
